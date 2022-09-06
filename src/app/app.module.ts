0b6b7afa94b0258874fce23eea83b829d6f96b3e
IvanVolkov42 <ivanv@choosewithme.com>
2022-09-06 12:11:37 +0400

    First 19 course sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import {AppRoutingModule} from "./app-routing.module";
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import {RecipeService} from "./services/recipe.service";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import {RecipeService} from "./services/recipe.service";
     BrowserModule,
     ReactiveFormsModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [ShoppingListService, RecipeService],
   bootstrap: [AppComponent]
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -7,30 +7,36 @@ import {Subject} from "rxjs";
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
-  private recipes: Array<Recipe> = [
-    new Recipe('Shnitzel',
-      'Tasty',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg',
-       [
-         new Ingredient('Meat', 1),
-         new Ingredient( 'Spices', 10)
-       ]),
-    new Recipe('Burger',
-      'Not so tasty',
-      'https://cdn.pixabay.com/photo/2020/02/08/09/05/burger-4829526_1280.jpg',
-      [
-        new Ingredient('Bread', 2),
-        new Ingredient( 'Meat', 1),
-        new Ingredient( 'Tomato', 1),
-        new Ingredient( 'Grass', 3),
-        new Ingredient( 'Cheese', 2),
+  // private recipes: Array<Recipe> = [
+  //   new Recipe('Shnitzel',
+  //     'Tasty',
+  //     'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg',
+  //      [
+  //        new Ingredient('Meat', 1),
+  //        new Ingredient( 'Spices', 10)
+  //      ]),
+  //   new Recipe('Burger',
+  //     'Not so tasty',
+  //     'https://cdn.pixabay.com/photo/2020/02/08/09/05/burger-4829526_1280.jpg',
+  //     [
+  //       new Ingredient('Bread', 2),
+  //       new Ingredient( 'Meat', 1),
+  //       new Ingredient( 'Tomato', 1),
+  //       new Ingredient( 'Grass', 3),
+  //       new Ingredient( 'Cheese', 2),
+  //
+  //     ]),
+  // ];
+  private recipes: Recipe[] = [];
 
-      ]),
-  ];
-
-  constructor(private slService: ShoppingListService) {
+  constructor(
+    private slService: ShoppingListService) {
   }
 
+  setRecipes( recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
   getRecipes() {
     return this.recipes.slice();
   }
